d0942f57ac829202ce394962b37e844ebc3fe3e8
mblob <thebluefrog1@gmail.com>
2017-12-13 10:03:05 +0100

    Initial Commit

---
+++ start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx6G -Xms6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+UseLargePagesInMetaspace -jar ./SpongeBootstrap-0.6.0.jar
